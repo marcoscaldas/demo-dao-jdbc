9a1583edfa196fd455e04d2af59f7caeeeaccae2
marcoscaldas <donarumo@hotmail.com>
2020-09-08 19:08:51 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,20 +4,25 @@ import java.util.Date;
 
 import com.mysql.fabric.xmlrpc.base.Data;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
 		
-		Seller seller = new Seller(21, "Bob","bob@gmail.com", new Date(), 3000.0, obj);
 		System.out.println(seller);
+		
+		
+		
+		
 
 	}
 
